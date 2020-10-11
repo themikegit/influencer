45ddac0fc90c020418b4e6c6220887cb7e3d87ea
themikegit <rostovniki5@gmail.com>
2020-10-11 22:42:42 +0200

    Updates

---
+++ precache-manifest.2f59b4cc0e4770add3869a2fe1b66fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cd4e8b40750e62d98f3863500c107c",
+    "url": "/insta_note/index.html"
+  },
+  {
+    "revision": "d56647a03aed9a8a1762",
+    "url": "/insta_note/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d56647a03aed9a8a1762",
+    "url": "/insta_note/static/js/2.5d25ce40.chunk.js"
+  },
+  {
+    "revision": "b841bd56561e46c9a4fa8cca6e5bd8e8",
+    "url": "/insta_note/static/js/2.5d25ce40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0c59624ad82d016cdf",
+    "url": "/insta_note/static/js/main.81a704e7.chunk.js"
+  },
+  {
+    "revision": "2c76f21e453b965a9ddd",
+    "url": "/insta_note/static/js/runtime-main.3893933d.js"
+  }
+]);
